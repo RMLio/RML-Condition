610c05f0968178ddbfd151c6475d8e52c5ab3c4b
andimou <anastasia.dimou@ugent.be>
2016-01-19 13:11:40 +0100

    Metric extractor

---
+++ src/main/java/be/ugent/mmlab/metric/extractor/AbstractMetricExtractor.java
@@ -0,0 +1,19 @@
+package be.ugent.mmlab.metric.extractor;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * RML Binding Condition
+ *
+ * @author andimou
+ */
+public class AbstractMetricExtractor {
+    //Log
+    private static final Logger log =
+            LogManager.getLogger(AbstractMetricExtractor.class);
+
+    public void extractMetrics() {
+    }
+
+}
