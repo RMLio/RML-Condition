07d9aa6f5fa94cbccc355c8a7ee1b084d3949421
Ben De Meester <ben.demeester@ugent.be>
2018-04-06 13:08:27 +0200

    namespace

--- src/main/java/be/ugent/mmlab/rml/processor/StdConditionProcessor.java
+++ src/main/java/be/ugent/mmlab/rml/processor/StdConditionProcessor.java
@@ -2,7 +2,7 @@ package be.ugent.mmlab.rml.processor;
 
 import be.ugent.mmlab.rml.condition.model.BindingCondition;
 import be.ugent.mmlab.rml.condition.model.Condition;
-import be.ugent.mmlab.rml.function.ConcreteFunctionProcessor;
+import be.ugent.mmlab.fno.java.ConcreteFunctionProcessor;
 import be.ugent.mmlab.rml.logicalsourcehandler.termmap.TermMapProcessor;
 import be.ugent.mmlab.rml.logicalsourcehandler.termmap.concrete.*;
 import be.ugent.mmlab.rml.model.PredicateObjectMap;
